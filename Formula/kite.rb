f15fee2ca320d9631a61defce70f158420c19f79
jreleaserbot <jreleaser@kordamp.org>
2025-12-21 14:23:56 +0000

    kite 0.2.0

--- Formula/kite.rb
+++ Formula/kite.rb
@@ -1,30 +1,28 @@
-# Generated with JReleaser 1.21.0 at 2025-12-21T09:13:16.676832405Z
+# Generated with JReleaser 1.21.0 at 2025-12-21T14:23:44.475047017Z
 
 class Kite < Formula
   desc "Multi-cloud Infrastructure as Code CLI"
   homepage "https://github.com/kitecorp/kite"
-  url "https://github.com/kitecorp/kite-cli/releases/download/0.1.0/kite-0.1.0.jar", :using => :nounzip
-  version "0.1.0"
-  sha256 "7d391eea87b7664cb1885a66c28e624ef8c1f0f972da9446a9c7a4b534a362b6"
+  version "0.2.0"
   license "Apache-2.0"
 
-  depends_on "openjdk" => "25"
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.0/kite-0.2.0-linux-x86_64.zip"
+    sha256 "ba7fc57ccd778ceb57a4b724b2ac11da9e0f7ac782ff37783aec5bc0ea52ca06"
+  end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.0/kite-0.2.0-osx-aarch64.zip"
+    sha256 "b55cb89a9932086f9a9602c5507a86330134a1a86d73bae0708fddfe10000f55"
+  end
+
 
   def install
-    libexec.install "kite-0.1.0.jar"
-
-    bin.mkpath
-    File.open("#{bin}/kite", "w") do |f|
-      f.write <<~EOS
-        #!/bin/bash
-        export JAVA_HOME="#{Language::Java.overridable_java_home_env(nil)[:JAVA_HOME]}"
-        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/kite-0.1.0.jar "$@"
-      EOS
-    end
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/bin/kite" => "kite"
   end
 
   test do
     output = shell_output("#{bin}/kite --version")
-    assert_match "0.1.0", output
+    assert_match "0.2.0", output
   end
 end
