e002827bfd4ebb1a5d6ff1d4f263c00c6237dc2c
jreleaserbot <jreleaser@kordamp.org>
2025-12-21 09:13:28 +0000

    kite 0.1.0

--- Formula/kite.rb
+++ Formula/kite.rb
@@ -1,9 +1,9 @@
-# Generated with JReleaser 1.21.0 at 2025-12-21T09:05:34.27667744Z
+# Generated with JReleaser 1.21.0 at 2025-12-21T09:13:16.676832405Z
 
 class Kite < Formula
   desc "Multi-cloud Infrastructure as Code CLI"
   homepage "https://github.com/kitecorp/kite"
-  url "https://github.com/kitecorp/kite/releases/download/v0.1.0/kite-0.1.0.jar", :using => :nounzip
+  url "https://github.com/kitecorp/kite-cli/releases/download/0.1.0/kite-0.1.0.jar", :using => :nounzip
   version "0.1.0"
   sha256 "7d391eea87b7664cb1885a66c28e624ef8c1f0f972da9446a9c7a4b534a362b6"
   license "Apache-2.0"
