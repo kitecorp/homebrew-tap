df6bfe72a136eab18b6722e7ee220514d6416d94
jreleaserbot <jreleaser@kordamp.org>
2025-12-21 17:57:54 +0000

    kite 0.2.2

--- Formula/kite.rb
+++ Formula/kite.rb
@@ -1,26 +1,22 @@
-# Generated with JReleaser 1.21.0 at 2025-12-21T16:14:50.799870838Z
+# Generated with JReleaser 1.21.0 at 2025-12-21T17:57:28.144749461Z
 
 class Kite < Formula
   desc "Multi-cloud Infrastructure as Code CLI"
   homepage "https://github.com/kitecorp/kite"
-  version "0.2.1"
+  version "0.2.2"
   license "Apache-2.0"
 
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.1/kite-0.2.1-linux-arm64.zip"
-    sha256 "4cbaa4a0709dbd255adf29cc35aaade48df397a7f6a35c1d48219aeef6c60793"
-  end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.1/kite-0.2.1-linux-amd64.zip"
-    sha256 "e81d3a27c3b6a5ba36b2a87675735d928a46859bc29dd8784baf76985c2c18cf"
+    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.2/kite-0.2.2-linux-amd64.zip"
+    sha256 "5d7db6a7cbdd4dde13664d437b08d17a3e0c9edcd12bd3fd709263b1d1efc8b7"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.1/kite-0.2.1-osx-arm64.zip"
-    sha256 "43b0c7222b6a173ac788c5934ee66aa2e51332062aa8c784db848239ae953a01"
+    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.2/kite-0.2.2-osx-arm64.zip"
+    sha256 "7226ad235c98db63a663fde39f2f4a4e67e20aa6f5a69fe79cbc45acbc912a77"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.1/kite-0.2.1-osx-amd64.zip"
-    sha256 "e2f4b97cfa8ed44a98086accbb5b41c59fb5e19873da560b33483254dd69cafb"
+    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.2/kite-0.2.2-osx-amd64.zip"
+    sha256 "6ef698c1fd0ca490f7d29ca3b6fa48c69cde8654709d5dbf16ad233f5301c412"
   end
 
 
@@ -31,6 +27,6 @@ class Kite < Formula
 
   test do
     output = shell_output("#{bin}/kite --version")
-    assert_match "0.2.1", output
+    assert_match "0.2.2", output
   end
 end
