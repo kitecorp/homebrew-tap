106b1f630b5a46c3f39682755e7ef314c0f7fdb3
jreleaserbot <jreleaser@kordamp.org>
2025-12-22 16:34:56 +0000

    kite 0.2.2

--- Formula/kite.rb
+++ Formula/kite.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.21.0 at 2025-12-21T17:57:28.144749461Z
+# Generated with JReleaser 1.21.0 at 2025-12-22T16:34:43.999227368Z
 
 class Kite < Formula
   desc "Multi-cloud Infrastructure as Code CLI"
@@ -6,17 +6,9 @@ class Kite < Formula
   version "0.2.2"
   license "Apache-2.0"
 
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.2/kite-0.2.2-linux-amd64.zip"
-    sha256 "5d7db6a7cbdd4dde13664d437b08d17a3e0c9edcd12bd3fd709263b1d1efc8b7"
-  end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/kitecorp/kite-cli/releases/download/0.2.2/kite-0.2.2-osx-arm64.zip"
-    sha256 "7226ad235c98db63a663fde39f2f4a4e67e20aa6f5a69fe79cbc45acbc912a77"
-  end
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.2/kite-0.2.2-osx-amd64.zip"
-    sha256 "6ef698c1fd0ca490f7d29ca3b6fa48c69cde8654709d5dbf16ad233f5301c412"
+    sha256 "eb1bf0d3d7b6dca86c2e334e040e6c8860fd1ea7a995f858139b96b4ecba2ffb"
   end
 
 
