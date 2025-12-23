2584ee1cfc78c035d6fc04357652023ff1a6d575
jreleaserbot <jreleaser@kordamp.org>
2025-12-23 10:32:26 +0000

    kite 0.2.3

--- Formula/kite.rb
+++ Formula/kite.rb
@@ -1,14 +1,14 @@
-# Generated with JReleaser 1.21.0 at 2025-12-23T01:17:29.642656819Z
+# Generated with JReleaser 1.21.0 at 2025-12-23T10:32:09.022191429Z
 
 class Kite < Formula
   desc "Multi-cloud Infrastructure as Code CLI"
   homepage "https://github.com/kitecorp/kite"
-  version "0.2.2"
+  version "0.2.3"
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.2/kite-0.2.2-osx-arm64.zip"
-    sha256 "c53c495d25cea21283a6a73793195380eb878515fcc7b6f4df7ed28665bf49f1"
+    url "https://github.com/kitecorp/kite-cli/releases/download/0.2.3/kite-0.2.3-osx-arm64.zip"
+    sha256 "f0d28c3410bf6fe1eebed2a3cbded7c612e89d54bbe5e92e2df2e2e69aae683a"
   end
 
 
@@ -19,6 +19,6 @@ class Kite < Formula
 
   test do
     output = shell_output("#{bin}/kite --version")
-    assert_match "0.2.2", output
+    assert_match "0.2.3", output
   end
 end
