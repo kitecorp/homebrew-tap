dc3a6590ff42af39730bf2ce32af94e049d84582
jreleaserbot <jreleaser@kordamp.org>
2026-01-11 01:19:07 +0000

    kite v0.3.11

--- Formula/kite.rb
+++ Formula/kite.rb
@@ -1,26 +1,26 @@
-# Generated with JReleaser 1.22.0 at 2026-01-10T04:45:04.905956901Z
+# Generated with JReleaser 1.22.0 at 2026-01-11T01:19:04.55666813Z
 
 class Kite < Formula
   desc "Multi-cloud Infrastructure as Code CLI"
   homepage "https://github.com/kitecorp/kite-language"
-  version "0.3.7"
+  version "0.3.11"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/kitecorp/kite-cli/releases/download/v0.3.7/kite-0.3.7-linux-arm64.zip"
-    sha256 "0c9917046545af389e9b4125e54a473eb2406ecc1fedca87f664fcd283a8ee30"
+    url "https://github.com/kitecorp/kite-cli/releases/download/v0.3.11/kite-0.3.11-linux-arm64.zip"
+    sha256 "78ff954e16aab1c45bbe83ed13be78153ed7d46de3b5b325ddaf20109cb29de3"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/kitecorp/kite-cli/releases/download/v0.3.7/kite-0.3.7-linux-amd64.zip"
-    sha256 "6d26815157448dd8e51e806ba04be46ab3492d04357413e998dc9f1b2dbc4b35"
+    url "https://github.com/kitecorp/kite-cli/releases/download/v0.3.11/kite-0.3.11-linux-amd64.zip"
+    sha256 "f792207b699a8edf9c26b46045ac148d94320b6dec9b004b728884845f3e6fdf"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/kitecorp/kite-cli/releases/download/v0.3.7/kite-0.3.7-osx-arm64.zip"
-    sha256 "c93f0506909b268a817ae44499afaca5cec93b6b2d2a407393a63adbe81edf5a"
+    url "https://github.com/kitecorp/kite-cli/releases/download/v0.3.11/kite-0.3.11-osx-arm64.zip"
+    sha256 "361ecba6aae4ff38438c8074193133bdbfa95abba4536659b1fca0ec867626f8"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/kitecorp/kite-cli/releases/download/v0.3.7/kite-0.3.7-osx-amd64.zip"
-    sha256 "f038087c6f278637294dac6dba87b3fca0e0c4583aee16ca76bb8eb08ad475fe"
+    url "https://github.com/kitecorp/kite-cli/releases/download/v0.3.11/kite-0.3.11-osx-amd64.zip"
+    sha256 "d8754569f1790ae846c07abf7b6dcdf839d439fbbbab724afca8aac6d934f725"
   end
 
 
@@ -31,6 +31,6 @@ class Kite < Formula
 
   test do
     output = shell_output("#{bin}/kite --version")
-    assert_match "0.3.7", output
+    assert_match "0.3.11", output
   end
 end
